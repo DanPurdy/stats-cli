4ee37c0e375e2f91a2090e52342fea2b3f2d9029
Dan Purdy <dan@dpurdy.me>
2023-08-13 10:02:16 +0100

    Add count merge requests comand

--- cmd/count_merge_requests.go
+++ cmd/count_merge_requests.go
@@ -2,11 +2,12 @@ package cmd
 
 import (
 	"fmt"
-	"net/http"
+	"log"
 	"os"
 	"time"
 
 	"github.com/spf13/cobra"
+	"github.com/xanzy/go-gitlab"
 )
 
 var countMergeRequestsCmd = &cobra.Command{
@@ -14,57 +15,62 @@ var countMergeRequestsCmd = &cobra.Command{
     Short: "Count the merge requests created by a user between two dates",
     Run: func(cmd *cobra.Command, args []string) {
         // Read token
-        token := os.Getenv("GITLAB_TOKEN")
+        token := os.Getenv("GITLAB_CLI_TOKEN")
         if token == "" {
-            fmt.Println("GITLAB_TOKEN environment variable is not set.")
+            fmt.Println("GITLAB_CLI_TOKEN environment variable is not set.")
             return
         }
 
+        git, err := gitlab.NewClient(token)
+        if err != nil {
+            log.Fatalf("Failed to create client: %v", err)
+        }
+
         // Prompt for username and dates
-        var username, startDate, endDate string
+        var username string
         fmt.Print("Enter GitLab username: ")
         fmt.Scanln(&username)
-        fmt.Print("Enter start date (YYYY-MM-DD): ")
-        fmt.Scanln(&startDate)
-        fmt.Print("Enter end date (YYYY-MM-DD): ")
-        fmt.Scanln(&endDate)
+        // fmt.Print("Enter start date (YYYY-MM-DD): ")
+        // fmt.Scanln(&startDate)
+        // fmt.Print("Enter end date (YYYY-MM-DD): ")
+        // fmt.Scanln(&endDate)
+
+        userOpt:= gitlab.SearchOptions{ListOptions: gitlab.ListOptions{PerPage: 1}};
+
+        users, _, err := git.Search.Users(username, &userOpt);
+        if err != nil {
+            log.Fatalf("Failed to get user: %v", err)
+        }
 
         // Convert dates to time.Time
-        start, err := time.Parse("2006-01-02", startDate)
+        start := time.Now().Add(-time.Hour * 720);
         if err != nil {
             fmt.Println("Error parsing start date:", err)
             return
         }
-        end, err := time.Parse("2006-01-02", endDate)
+        end := time.Now()
         if err != nil {
             fmt.Println("Error parsing end date:", err)
             return
         }
 
-        // Send request to GitLab API
-        url := fmt.Sprintf("%s/merge_requests?scope=all&state=all&author_username=%s&created_after=%s&created_before=%s", "gitlabAPIURL", username, start.Format(time.RFC3339), end.Format(time.RFC3339))
-        client := &http.Client{}
-        req, err := http.NewRequest("GET", url, nil)
-        if err != nil {
-            fmt.Printf("Error creating request: %v\n", err)
-            return
+        opt:= gitlab.ListMergeRequestsOptions{
+            ListOptions: gitlab.ListOptions{PerPage: 1},
+            AuthorID: gitlab.Int(users[0].ID),
+            CreatedAfter: gitlab.Time(start), 
+            CreatedBefore: gitlab.Time(end),
+            Scope: gitlab.String("all"),
         }
 
-        // Set the authorization header
-        req.Header.Set("Authorization", "Bearer "+token)
-
-        resp, err := client.Do(req)
+        _, resp2, err := git.MergeRequests.ListMergeRequests(&opt)
         if err != nil {
-            fmt.Printf("Error making request: %v\n", err)
-            return
+            log.Fatalf("Failed to list merge requests: %v", err)
         }
-        defer resp.Body.Close()
 
-        // Get the X-Total header
-        xTotal := resp.Header.Get("X-Total")
+        xTotal:= resp2.Header.Get("X-Total");
 
         // Output the number of merge requests
-        fmt.Printf("Number of merge requests created by %s between %s and %s: %s\n", username, startDate, endDate, xTotal)
+        fmt.Printf("Number of merge requests created by %s between %s and %s: %s\n", username, start, end, xTotal)
     },
 }
 
