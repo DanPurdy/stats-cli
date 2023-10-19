aaae6c067ea82e6451218743f13f520da9463bb6
Dan Purdy <dan@dpurdy.me>
2023-10-19 07:53:48 +0100

    update merge request count

--- cmd/count_merge_requests.go
+++ cmd/count_merge_requests.go
@@ -8,12 +8,17 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/xanzy/go-gitlab"
+	bolt "go.etcd.io/bbolt"
 )
 
 var countMergeRequestsCmd = &cobra.Command{
     Use:   "count-merge-requests",
     Short: "Count the merge requests created by a user between two dates",
     Run: func(cmd *cobra.Command, args []string) {
+        db, err := bolt.Open("dev-stats.db", 0600, nil);
+        if err != nil {
+            log.Fatal(err)
+        }
         // Read token
         token := os.Getenv("GITLAB_CLI_TOKEN")
         if token == "" {
@@ -27,13 +32,13 @@ var countMergeRequestsCmd = &cobra.Command{
         }
 
         // Prompt for username and dates
-        var username string
+        var username, startDate, endDate string
         fmt.Print("Enter GitLab username: ")
         fmt.Scanln(&username)
-        // fmt.Print("Enter start date (YYYY-MM-DD): ")
-        // fmt.Scanln(&startDate)
-        // fmt.Print("Enter end date (YYYY-MM-DD): ")
-        // fmt.Scanln(&endDate)
+        fmt.Print("Enter start date (YYYY-MM-DD): ")
+        fmt.Scanln(&startDate)
+        fmt.Print("Enter end date (YYYY-MM-DD): ")
+        fmt.Scanln(&endDate)
 
         userOpt:= gitlab.SearchOptions{ListOptions: gitlab.ListOptions{PerPage: 1}};
 
@@ -43,12 +48,14 @@ var countMergeRequestsCmd = &cobra.Command{
         }
 
         // Convert dates to time.Time
-        start := time.Now().Add(-time.Hour * 720);
+        startParsed, err := time.Parse("2006-01-02", startDate)
+        start := time.Date(startParsed.Year(), startParsed.Month(), startParsed.Day(), 0, 0, 0, 0, time.UTC)
         if err != nil {
             fmt.Println("Error parsing start date:", err)
             return
         }
-        end := time.Now()
+        endParsed, err := time.Parse("2006-01-02", endDate)
+        end := time.Date(endParsed.Year(), endParsed.Month(), endParsed.Day(), 0, 0, 0, 0, time.UTC)
         if err != nil {
             fmt.Println("Error parsing end date:", err)
             return
@@ -71,6 +78,11 @@ var countMergeRequestsCmd = &cobra.Command{
 
         // Output the number of merge requests
         fmt.Printf("Number of merge requests created by %s between %s and %s: %s\n", username, start, end, xTotal)
+        err := db.Update(func(tx *bolt.Tx) error {
+            tx.wr
+            return nil
+        })
+        defer db.Close()
     },
 }
 
